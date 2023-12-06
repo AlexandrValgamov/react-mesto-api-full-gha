43008b8ff5dc3d132dcbedab08f9a9c7bae197d5
Alexandr Valgamov <valgamov2010@yandex.ru>
2023-12-06 22:02:33 +0500

    fix auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -13,9 +13,9 @@ const auth = (req, res, next) => {
     const validToken = token.replace('Bearer ', '');
     payload = jwt.verify(validToken, NODE_ENV ? JWT_SECRET : '980758d3d66cd05999d3bf561c2e242bb093fcb6006061b4ac06ced07d36a620');
   } catch (error) {
-    next(new UnauthorizedError('С токеном что-то не так'));
+    return next(new UnauthorizedError('С токеном что-то не так'));
   }
-
+  console.log(payload);
   req.user = payload;
   next();
 };
